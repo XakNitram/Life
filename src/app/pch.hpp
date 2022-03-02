e394aec6a90176c4a4b0163248beb29494689a65
Tate Mioton <tatemioton@outlook.com>
2022-03-02 16:10:29 -0600

    Add Precompiled Headers

---
+++ src/app/pch.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <lwvl/lwvl.hpp>
+
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
